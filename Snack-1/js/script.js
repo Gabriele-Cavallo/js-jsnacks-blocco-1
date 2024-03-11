491b95075ae76adf4422b8d21bc11bc955b4b15c
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 13:42:55 +0100

    feat: add array snack-1

--- Snack-1/js/script.js
+++ Snack-1/js/script.js
@@ -1,1 +1,7 @@
 // - Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+// 1 - Creo l'array di numeri
+const numbers = [];
+console.log('numbers-array' , numbers);
+// 2 - Chiedo all'utente 5 numeri
+// 3 - Per ogni numeri se non è presente nell'arrey lo aggiungo
