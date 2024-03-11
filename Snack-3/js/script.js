9bbdc515472363a9ebee98625ebac28cc30c055e
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 14:00:15 +0100

    feat: add for cicle snack-3

--- Snack-3/js/script.js
+++ Snack-3/js/script.js
@@ -3,6 +3,11 @@
 //   se è dispari inseriscilo nell’array.
 
 // 1 - Creo un array vuoto
-const number = [];
+const numbers = [];
+console.log('array' , numbers);
 // 2 - Chiedo all'utente 6 numeri
-    // -Se l'utente inserisce un numero dispari lo inserisco nell'array     
+    // -Se l'utente inserisce un numero dispari lo inserisco nell'array    
+for (let i = 0; i < 6; i++) {
+    let userNumber = parseInt(prompt('Dimmi un numero'));
+    console.log('user-number' , userNumber);
+}     
