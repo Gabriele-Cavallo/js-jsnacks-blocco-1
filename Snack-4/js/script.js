c9e743b7bee25e48bfa0441462a38952a98e8d18
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 14:26:49 +0100

    feat: add array method to snack 4

--- Snack-4/js/script.js
+++ Snack-4/js/script.js
@@ -1,13 +1,27 @@
 // - Calcola la somma e la media dei primi 10 numeri.
 
+// 1-Metodo senza array
 // 1 - Calcolo la somma dei primi 10 numeri
     // Faccio la media dei primi 10 numeri
+// sum = 0;
+// let avarage = 10;
+// for (let i = 1; i <= avarage; i++) {
+//     let number = i;
+//     sum += number;
+// }
+// console.log('sum' , sum);
+// let avarageNumber = sum / avarage ;
+// console.log('avarage-number' , avarageNumber);
+
+// 2-Metodo con array
+const avarage = [];
 sum = 0;
-let avarage = 10;
-for (let i = 1; i <= avarage; i++) {
+for (let i = 1; i <= 10; i++) {
     let number = i;
+    avarage.push(number);
     sum += number;
 }
 console.log('sum' , sum);
-let avarageNumber = sum / avarage ;
-console.log('avarage-number' , avarageNumber);
+let avarageNumber = sum / avarage.length;
+console.log('avarage' , avarageNumber);
+console.log('avarage' , avarage);
