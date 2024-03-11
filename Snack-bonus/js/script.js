aef1be6bf7d790fc75c9134511d54426e30707c1
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 14:39:00 +0100

    feat: add for cicle snack bonus

--- Snack-bonus/js/script.js
+++ Snack-bonus/js/script.js
@@ -1,2 +1,13 @@
 // BONUS:
 // - Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+// 1 - Creo un array di 10 numeri
+const numbers = [];
+for (let i = 0; i < 10; i++) {
+    let numb = Math.floor((Math.random() * 100) + 1);
+    numbers.push(numb);
+    console.log('random-number' , numb);
+}
+console.log('array' , numbers);
+// 2 - Controllo i numeri raccolti
+// 3 - Stampo in console il più grande
