4fbb9b8c6c3085c4b9bb0d271373d4aa68410a47
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 14:18:31 +0100

    feat: add avarage number

--- Snack-4/js/script.js
+++ Snack-4/js/script.js
@@ -2,9 +2,12 @@
 
 // 1 - Calcolo la somma dei primi 10 numeri
     // Faccio la media dei primi 10 numeri
-sum=0;
-for (let i = 1; i <= 10; i++) {
+sum = 0;
+let avarage = 10;
+for (let i = 1; i <= avarage; i++) {
     let number = i;
     sum += number;
-    console.log('sum' , sum);
 }
+console.log('sum' , sum);
+let avarageNumber = sum / avarage ;
+console.log('avarage-number' , avarageNumber);
