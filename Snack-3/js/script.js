a1b05e5194be4088078290156b39811825c3ee99
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 14:03:33 +0100

    feat: add if condition snack-3

--- Snack-3/js/script.js
+++ Snack-3/js/script.js
@@ -6,8 +6,11 @@
 const numbers = [];
 console.log('array' , numbers);
 // 2 - Chiedo all'utente 6 numeri
-    // -Se l'utente inserisce un numero dispari lo inserisco nell'array    
+// -Se l'utente inserisce un numero dispari lo inserisco nell'array    
 for (let i = 0; i < 6; i++) {
     let userNumber = parseInt(prompt('Dimmi un numero'));
     console.log('user-number' , userNumber);
+    if(userNumber % 2 !== 0){
+        numbers.push(userNumber);
+    }
 }     
