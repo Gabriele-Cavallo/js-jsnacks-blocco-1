8cdc711f2d084e7a18849fbee0db5d12607a49ae
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 13:52:20 +0100

    feat: add for cicle snack-2

--- Snack-2/js/script.js
+++ Snack-2/js/script.js
@@ -1,2 +1,10 @@
 // - Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //   Il programma stampa la somma di tutti i numeri inseriti.
+
+// 1 - Chiedo all'utente 5 numeri
+for (let i = 0; i < 5; i++) {
+    let userNumber = parseInt(prompt('Dimmi un numero'));
+    console.log('user-number' , userNumber);
+}
+// 2 - Sommo i numeri inseriti dall'utente
+// 3 - Stampo in console il risultato
