e517285c5d81b6f329bf874a8f2a5e9869454e0d
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 13:54:15 +0100

    feat: add sum variable

--- Snack-2/js/script.js
+++ Snack-2/js/script.js
@@ -2,9 +2,11 @@
 //   Il programma stampa la somma di tutti i numeri inseriti.
 
 // 1 - Chiedo all'utente 5 numeri
+// 2 - Sommo i numeri inseriti dall'utente
+sum = 0;
 for (let i = 0; i < 5; i++) {
     let userNumber = parseInt(prompt('Dimmi un numero'));
     console.log('user-number' , userNumber);
+    sum += userNumber
 }
-// 2 - Sommo i numeri inseriti dall'utente
 // 3 - Stampo in console il risultato
