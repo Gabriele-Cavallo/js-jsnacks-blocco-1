b2de9a8d2b144de43852c56594392c8d3b41538a
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-11 14:09:03 +0100

    feat: add for cicle snack-4

--- Snack-4/js/script.js
+++ Snack-4/js/script.js
@@ -1,1 +1,10 @@
 // - Calcola la somma e la media dei primi 10 numeri.
+
+// 1 - Calcolo la somma dei primi 10 numeri
+    // Faccio la media dei primi 10 numeri
+sum=0;
+for (let i = 1; i <= 10; i++) {
+    let number = i;
+    sum += number;
+    console.log('sum' , sum);
+}
